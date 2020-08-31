d17646044f50400ea00f2e370c9feebee7e619a1
Mauro Dorigo <mau.dorigo@outlook.es>
2020-08-31 18:10:36 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -6,7 +6,8 @@ PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
-def assembled_aoh [
+def assembled_aoh 
+  [
   DON_G,
   JOELLE_VD,
   PAT_M,
