7da7b3c7a7adbc102dfdf6b7deeae1e2e035dd53
Mauro Dorigo <mau.dorigo@outlook.es>
2020-08-31 18:11:48 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -17,8 +17,10 @@ def assembled_aoh
 end
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
+  [ { name:  "Don Gately", occupation: "Live-in Staff" },
+  
+    
+    ]
 end
 
 def aoh_lookup(aoh, row, key)
